811761bd17883f45de35b9f874e83ad816fbcf4d
ekidhaja <ekidhaja@gmail.com>
2022-03-26 17:55:44 +0100

    Searchbar design done

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,9 +1,11 @@
+import Body from "./Body";
 import Sidebar from "./Sidebar";
 
 const Dashboard = () => {
     return (
         <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
             <Sidebar />
+            <Body />
         </main>
     );
 }
