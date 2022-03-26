edeaf2785065c3d2c78f294df9209fd77ab638f0
ekidhaja <ekidhaja@gmail.com>
2022-03-26 20:20:00 +0100

    Sign in using spotify provider done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Dashboard from '../components/Dashboard'
-import styles from '../styles/Home.module.css'
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Dashboard from "../components/Dashboard";
+import Loader from "../components/Loader";
 
 export default function Home() {
+
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  // Loading animation...
+  if (status === "loading") {
+    return <Loader />;
+  }
+  
   return (
     <div className="">
       <Head>
