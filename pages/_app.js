7f15be0de3a2e174b4e7f7f5b333321805b2c906
ekidhaja <ekidhaja@gmail.com>
2022-03-26 20:34:04 +0100

    Revert "Sign in using spotify provider done"
    This reverts commit edeaf2785065c3d2c78f294df9209fd77ab638f0.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-import '../styles/globals.css';
-import { SessionProvider } from "next-auth/react";
-import { RecoilRoot } from "recoil";
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </SessionProvider>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
