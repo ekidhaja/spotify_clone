b3bf42f866ce0ceb02b1ea46bea2e4808b66cda5
ekidhaja <ekidhaja@gmail.com>
2022-03-27 02:17:22 +0100

    Search feature done and added genres, tracks, and latest releases

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,16 +2,24 @@ import Body from "./Body";
 import Sidebar from "./Sidebar";
 import Right from "./Right";
 import SpotifyWebApi from "spotify-web-api-node";
+import { playingTrackState } from "../atoms/playerAtom";
+import { useRecoilState } from "recoil";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
 const Dashboard = () => {
+    const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+    const chooseTrack = (track) => {
+        setPlayingTrack(track);
+    };
+
     return (
         <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
             <Sidebar />
-            <Body />
+            <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
             <Right />
         </main>
     );
