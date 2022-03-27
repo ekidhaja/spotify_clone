867b62751edc8fa1a05b42263fa742394a1c0617
ekidhaja <ekidhaja@gmail.com>
2022-03-27 02:47:27 +0100

    right section done and added recently played tracks

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,12 +4,16 @@ import Right from "./Right";
 import SpotifyWebApi from "spotify-web-api-node";
 import { playingTrackState } from "../atoms/playerAtom";
 import { useRecoilState } from "recoil";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
 const Dashboard = () => {
+    const { data: session } = useSession();
+    const { accessToken } = session;
     const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
 
     const chooseTrack = (track) => {
@@ -20,7 +24,7 @@ const Dashboard = () => {
         <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
             <Sidebar />
             <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
-            <Right />
+            <Right spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
         </main>
     );
 }
